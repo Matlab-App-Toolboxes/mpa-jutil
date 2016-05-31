2777ab8ea935baa617705ef0974c7a3e8700e3bb
ragavsathish <ragav.sathish@gmail.com>
2016-05-31 11:40:43 +0300

    removed persistence factory
    Persistence factory is removed and all the functionalities has been
    pushed to JAXBUtil.

---
+++ src/main/java/io/mpa/orm/util/JAXBUtil.java
@@ -0,0 +1,17 @@
+package io.mpa.orm.util;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+public class JAXBUtil {
+
+	public static Object unMarshal(String path, Class<?> className) throws JAXBException {
+		JAXBContext jaxbContext = JAXBContext.newInstance(className);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		return jaxbUnmarshaller.unmarshal(new File(path));
+	}
+
+}
